7581e2b3b29f4ce73b127390cf0181aa7336ea37
kyang12 <kyang12@ncsu.edu>
2018-04-13 10:48:57 -0400

    create hotel in console

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -29,9 +29,30 @@ public class Constants {
     public static final String ERROR_CHECK_IN_ROOM_UNAVAILABLE;
     public static final String ERROR_CHECK_IN_EXCEED_OCCUPANCY;
     public static final String ERROR_SERVICE_INVALID_CHECK_IN;
+    public static final String ERROR_CONSOLE_INVALID_PARAMETER;
     public static final float PARAMETER_DISCOUNT;
+    public static final String CONSOLE_MARKER_COMMAND;
+    public static final String CONSOLE_MARKER_PARAMETER;
+    public static final String PROMPT_MENU;
+    public static final String PROMPT_CREATE;
+    public static final String PROMPT_READ;
+    public static final String PROMPT_UPDATE;
+    public static final String PROMPT_DELETE;
+    public static final String PROMPT_ASSIGN;
+    public static final String PROMPT_RELEASE;
+    public static final String PROMPT_PARAMETER_HOTEL;
     public static final String CMD_MENU = "menu";
     public static final String CMD_EXIT = "exit";
+    public static final String CMD_CREATE = "enter";
+    public static final String CMD_READ = "check";
+    public static final String CMD_UPDATE = "update";
+    public static final String CMD_DELETE = "delete";
+    public static final String CMD_ASSIGN = "assign";
+    public static final String CMD_RELEASE = "release";
+    public static final String CMD_OBJECT_HOTEL = "hotel";
+    public static final String CMD_OBJECT_ROOM = "room";
+    public static final String CMD_OBJECT_STAFF = "staff";
+    public static final String CMD_OBJECT_CUSTOMER = "customer";
     static{
         ResourceBundle config = ResourceBundle.getBundle("config");
         ResourceBundle account = ResourceBundle.getBundle("account");
@@ -55,6 +76,17 @@ public class Constants {
         ERROR_CHECK_IN_ROOM_UNAVAILABLE = config.getString("error.checkIn.roomUnavailable");
         ERROR_CHECK_IN_EXCEED_OCCUPANCY = config.getString("error.checkIn.exceedOccupancy");
         ERROR_SERVICE_INVALID_CHECK_IN = config.getString("error.service.invalidCheckIn");
+        ERROR_CONSOLE_INVALID_PARAMETER = config.getString("error.console.invalidParameter");
         PARAMETER_DISCOUNT = Float.parseFloat(config.getString("parameter.discount"));
+        CONSOLE_MARKER_COMMAND = config.getString("console.marker.command");
+        CONSOLE_MARKER_PARAMETER = config.getString("console.marker.parameter");
+        PROMPT_MENU = config.getString("prompt.menu");
+        PROMPT_CREATE = config.getString("prompt.create");
+        PROMPT_READ = config.getString("prompt.read");
+        PROMPT_UPDATE = config.getString("prompt.update");
+        PROMPT_DELETE = config.getString("prompt.delete");
+        PROMPT_ASSIGN = config.getString("prompt.assign");
+        PROMPT_RELEASE = config.getString("prompt.release");
+        PROMPT_PARAMETER_HOTEL = config.getString("prompt.parameter.hotel");
     }
 }
--- src/ui/Console.java
+++ src/ui/Console.java
@@ -1,36 +1,104 @@
 package ui;
 
+import common.InfoProcess;
+import common.Maintainance;
+import common.Report;
+import db.Database;
+import model.Model;
+import sun.applet.Main;
+
 import java.io.*;
 
 import static common.Constants.*;
 
 public class Console {
-    static final String ABC = "menu";
     InputStream in;
+    BufferedReader br;
     PrintStream out;
 
     public Console(InputStream in, PrintStream out) {
         this.in = in;
         this.out = out;
+        this.br = new BufferedReader(new InputStreamReader(this.in));
     }
 
-    public void menu() {
-        // TODO: Print menu
+    /**
+     * Print menu
+     * @param args
+     */
+    public void menu(String[] args) {
+        out.println(PROMPT_MENU);
     }
 
-    public void launch() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(this.in));
-        for(String line = br.readLine(); !CMD_EXIT.equalsIgnoreCase(line); line = br.readLine()) {
-            String[] command = line.split("\\s+");
-            switch (command[0]) {
-                case CMD_MENU: menu(); break;
-                // TODO: add commands
-            }
+    public void create(String[] args) {
+        if(args.length < 2) out.println(ERROR_CONSOLE_INVALID_PARAMETER);
+        out.println(PROMPT_CREATE);
+        switch (args[1].toLowerCase()) {
+            case CMD_OBJECT_HOTEL: createHotel(args); return;
+            default: out.println(ERROR_CONSOLE_INVALID_PARAMETER);
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        Console console = new Console(System.in, System.out);
-        console.launch();
+    public void createHotel(String[] args) {
+        // Print parameter detail
+        out.println(PROMPT_PARAMETER_HOTEL);
+        out.print(CONSOLE_MARKER_PARAMETER);
+
+        // Accept parameter and execute
+        String[] parameters;
+        try {
+            parameters = br.readLine().split(",");
+            InfoProcess.createHotel(parameters[0].trim(), parameters[1].trim(), parameters[2].trim(), parameters[3].trim());
+        } catch (Exception e) {
+            out.println(ERROR_CONSOLE_INVALID_PARAMETER);
+        }
+    }
+
+    public void read(String[] args) {
+        // TODO: Print parameter detail
+
+        // TODO: accept further parameter and execute
+    }
+
+    public void update(String[] args) {
+        // TODO: Print parameter detail
+
+        // TODO: accept further parameter and execute
+    }
+
+    public void delete(String[] args) {
+        // TODO: Print parameter detail
+
+        // TODO: accept further parameter and execute
+    }
+
+    public void launch() throws IOException {
+        menu(new String[0]);
+        out.print(CONSOLE_MARKER_COMMAND);
+        for(String line = br.readLine(); !CMD_EXIT.equalsIgnoreCase(line); line = br.readLine()) {
+            String[] command = line.split("\\s+");
+            switch (command[0].toLowerCase()) {
+                case CMD_MENU: menu(command); break;
+                case CMD_CREATE: create(command); break;
+                case CMD_READ: read(command); break;
+                case CMD_UPDATE: update(command); break;
+                case CMD_DELETE: delete(command); break;
+                // TODO: add commands
+            }
+            out.print(CONSOLE_MARKER_COMMAND);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Database db = new Database(DB_DRIVER, DB_URL, DB_USER, DB_PASSWORD);
+        try {
+            Model.setDatabase(db);
+            InfoProcess.setDatabase(db);
+            Report.setDatabase(db);
+            Console console = new Console(System.in, System.out);
+            console.launch();
+        } finally {
+            db.close();
+        }
     }
 }
